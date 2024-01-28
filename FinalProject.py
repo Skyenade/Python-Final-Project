c9f8f24c287768fb5cdec77525d407651d7b589c
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-01-28 02:42:39 -0500

    creating tables

--- FinalProject.py
+++ FinalProject.py
@@ -1,4 +1,39 @@
-# test
-print("Hello World")
-print("what about now")
-# test merge
+import sqlite3
+
+def connect():
+    return sqlite3.connect('librarysystem.db')
+
+class LibraryManagementSystem:
+    def create_tables_(connection):
+        cursor = connection.cursor()
+        cursor.execute('''
+            create table if not exists librarians(
+            username text not null,
+            password text,
+            name text,
+            contact_information text
+            )  
+            ''')
+        cursor.execute('''
+            create table if not exists publisher(
+            publisher_name text,
+            address text,
+            contact_information text
+            )  
+            ''')
+        cursor.execute('''
+            create table if not exists users(
+            id integer primary key autoincrement,
+            name text not null,
+            age integer
+            )  
+            ''')
+        cursor.execute('''
+            create table if not exists users(
+            id integer primary key autoincrement,
+            name text not null,
+            age integer
+            )  
+            ''')
+        connection.commit()
+        cursor.close()
