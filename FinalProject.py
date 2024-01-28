5c0c687a302390bf5d104011d03eda7fdfa2897d
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-01-28 03:16:50 -0500

    finished the tables

--- FinalProject.py
+++ FinalProject.py
@@ -7,8 +7,9 @@ class LibraryManagementSystem:
     def create_tables_(connection):
         cursor = connection.cursor()
         cursor.execute('''
-            create table if not exists librarians(
-            username text not null,
+            create table if not exists librarian(
+            librarian_id integer primary key autoincrement,
+            username text,
             password text,
             name text,
             contact_information text
@@ -16,24 +17,46 @@ class LibraryManagementSystem:
             ''')
         cursor.execute('''
             create table if not exists publisher(
+            publisher_id integer primary key autoincrement,
             publisher_name text,
             address text,
+            contact_details text
+            )  
+            ''')
+        cursor.execute('''
+            create table if not exists book(
+            book_id integer primary key autoincrement,
+            title text,
+            author text,
+            genre text,
+            ISBN integer,
+            quantity integer,
+            publication_year integer
+            )  
+            ''')
+        cursor.execute('''
+            create table if not exists user(
+            user_id integer primary key autoincrement,
+            username text,
+            password text,
+            name text,
             contact_information text
             )  
             ''')
         cursor.execute('''
-            create table if not exists users(
-            id integer primary key autoincrement,
-            name text not null,
-            age integer
-            )  
-            ''')
-        cursor.execute('''
-            create table if not exists users(
-            id integer primary key autoincrement,
-            name text not null,
-            age integer
+            create table if not exists transaction(
+            user_id integer,
+            book_id integer,
+            due_date date,
+            contact_information text,
+            foreign key (user_id) references user (user_id),
+            foreign key (book_id) references book (book_id),
+            foreign key (contact_information) references user (contact_information),
             )  
             ''')
         connection.commit()
         cursor.close()
+
+class Librarian:
+    def __init__(self) -> None:
+        pass
