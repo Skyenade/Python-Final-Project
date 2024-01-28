d1ea65b1b2cef247e75a3363920293d198c34c17
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-01-28 04:12:30 -0500

    book class

--- FinalProject.py
+++ FinalProject.py
@@ -4,7 +4,7 @@ def connect():
     return sqlite3.connect('librarysystem.db')
 
 class LibraryManagementSystem:
-    def create_tables_(connection):
+    def create_tables(self,connection):
         cursor = connection.cursor()
         cursor.execute('''
             create table if not exists librarian(
@@ -58,14 +58,14 @@ class LibraryManagementSystem:
         cursor.close()
 
 class Librarian:
-    def __init__(self):
-        self.librarian = []
+    def __init__(self, connection):
+        self.connection = connection
 
     def add_librarian(self,username,password,name,contact_information):
         cursor = self.connection.cursor()
         cursor.execute('INSERT INTO librarian (username, password, name, contact_information) VALUES (?, ?, ?, ?)',
                        (username, password, name, contact_information))
-        self.conn.commit()
+        self.connection.commit()
         
     def edit_librarian(self,new_password,new_contact_information,username):
         cursor = self.connection.cursor()
@@ -75,23 +75,23 @@ class Librarian:
         elif new_contact_information:
             cursor.execute('UPDATE librarian SET contact_information = ? WHERE username = ?',
                        (new_contact_information,username))
-        self.conn.commit()
+        self.connection.commit()
 
     def delete_librarian(self,username):
         cursor = self.connection.cursor()
-        cursor.execute('DELETE FROM librarian username = ?',
+        cursor.execute('DELETE FROM librarian WHERE username = ?',
                        (username,))
-        self.conn.commit()
+        self.connection.commit()
 
 class Publisher:
-    def __init__(self):
-        self.librarian = []
+    def __init__(self, connection):
+        self.connection = connection
 
     def add_publisher(self,publisher_name,address,contact_details):
         cursor = self.connection.cursor()
         cursor.execute('INSERT INTO publisher (publisher_name,address,contact_details) VALUES (?, ?, ?)',
                        (publisher_name,address,contact_details))
-        self.conn.commit()
+        self.connection.commit()
         
     def edit_publisher(self,publisher_name,new_address,new_contact_details):
         cursor = self.connection.cursor()
@@ -101,10 +101,49 @@ class Publisher:
         elif new_contact_details:
             cursor.execute('UPDATE publisher SET contact_details = ? WHERE publisher_name = ?',
                        (new_contact_details,publisher_name))
-        self.conn.commit()
+        self.connection.commit()
 
     def delete_publisher(self,publisher_name):
         cursor = self.connection.cursor()
-        cursor.execute('DELETE FROM publisher publisher_name = ?',
+        cursor.execute('DELETE FROM publisher WHERE publisher_name = ?',
                        (publisher_name,))
-        self.conn.commit()        
+        self.connection.commit()
+
+class Book:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def add_book(self,title,author,genre,ISBN,quantity,publication_year):
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO book (title,author,genre,ISBN,quantity,publication_year) VALUES (?, ?, ?, ?, ?, ?)',
+                       (title,author,genre,ISBN,quantity,publication_year))
+        self.connection.commit()
+        
+    def edit_book(self,new_title,new_author,new_genre,new_ISBN,new_quantity,new_publication_year,book_id):
+        cursor = self.connection.cursor()
+        if new_title:
+            cursor.execute('UPDATE publisher SET title = ? WHERE book_id = ?',
+                       (new_title,book_id))
+        elif new_author:
+            cursor.execute('UPDATE publisher SET author = ? WHERE book_id = ?',
+                       (new_author,book_id))
+        elif new_genre:
+            cursor.execute('UPDATE publisher SET genre = ? WHERE book_id = ?',
+                       (new_genre,book_id))
+        elif new_ISBN:
+            cursor.execute('UPDATE publisher SET ISBN = ? WHERE book_id = ?',
+                       (new_ISBN,book_id))
+        elif new_quantity:
+            cursor.execute('UPDATE publisher SET quantity = ? WHERE book_id = ?',
+                       (new_quantity,book_id))
+        elif new_publication_year:
+            cursor.execute('UPDATE publisher SET publication_year = ? WHERE book_id = ?',
+                       (new_publication_year,book_id))  
+        self.connection.commit()
+        
+
+    def delete_book(self,book_id):
+        cursor = self.connection.cursor()
+        cursor.execute('DELETE FROM book WHERE book_id = ?',
+                       (book_id,))
+        self.connection.commit()
